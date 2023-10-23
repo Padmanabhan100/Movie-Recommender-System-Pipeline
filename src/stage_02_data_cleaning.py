97cfcd1b6e683081163fcb69c392a8b765c09f3e
Paddy <padmanabhanpaddy10@gmail.com>
2023-10-23 21:24:41 +0530

    stop tracking movieflix\artifacts\train_data_dir\train.csv

--- src/stage_02_data_cleaning.py
+++ src/stage_02_data_cleaning.py
@@ -11,40 +11,15 @@ def clean_data(path):
     raw_local_data_dir = config['artifacts']['raw_local_data_dir']
 
     # Get the list of names of the datasets to load
-    datasets = config['artifacts']['datasets']
+    dataset_name = config['artifacts']['dataset_name']
 
-    # Load the data as dataframes for processing
-    dataframes = []
+    
+    # Get the absolute path of the dataset
+    dataset_abs_path = f"{raw_local_data_dir}\{dataset_name}"
 
-    for dataset in datasets:
-        # Get the absolute path of the dataset
-        dataset_abs_path = f"{raw_local_data_dir}\{dataset}"
+    # Load the dataset into dataframe
+    ratings = pd.read_csv(dataset_abs_path) 
 
-        if dataset == 'movies_md.csv':
-            # Load the dataset into dataframe
-            df = pd.read_csv(dataset_abs_path, low_memory=False) 
-        else:
-            # Load the dataset into dataframe
-            df = pd.read_csv(dataset_abs_path) 
-
-        # Append to dataframes list
-        dataframes.append(df)
-
-    # Separate the dataframes
-    ratings, movie_md = dataframes
-
-    # Cleaning the dataset
-    # movie dataframe with votes more than 55
-    movie_md = movie_md[movie_md['vote_count']>55][['id','title']]
-
-    # IDs of movies with count more than 55
-    movie_ids = [int(x) for x in movie_md['id'].values]
-
-    # Select ratings of movies with more than 55 counts
-    ratings = ratings[ratings['movieId'].isin(movie_ids)]
-
-    # Reset Index
-    ratings.reset_index(inplace=True, drop=True)
 
     # Fetch the location of the file to save this clean data
     clean_local_data_dir = config['artifacts']['clean_local_data_dir']
