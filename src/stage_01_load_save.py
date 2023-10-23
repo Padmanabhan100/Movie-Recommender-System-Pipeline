24fd0efd807ead3c8d432a2b2c702a39ef090c97
Paddy <padmanabhanpaddy10@gmail.com>
2023-10-23 21:24:15 +0530

    stop tracking movieflix\artifacts\clean_local_data_dir\clean_data.csv

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -10,7 +10,6 @@ def get_data(path):
 
     # Fecthing the raw data path from the param.yaml
     raw_data_path1 = config['data_source']['ratings']
-    raw_data_path2 = config['data_source']['movies_metadata']
 
     # Fetch the path of the raw_local_data_dir and artifacts_dir
     raw_local_data_dir = config['artifacts']['raw_local_data_dir']
@@ -22,11 +21,9 @@ def get_data(path):
 
     # Read the data from the remote source 
     ratings = pd.read_csv(raw_data_path1)
-    movies_md = pd.read_csv(raw_data_path2, low_memory=False)
 
     # Save the loaded data into the raw_local_data_dir
     save_local_df(ratings, raw_local_data_dir, header=True, name='ratings.csv')
-    save_local_df(movies_md, raw_local_data_dir, header=True, name='movies_md.csv')
 
 def main(path='params.yaml'):
     try:
